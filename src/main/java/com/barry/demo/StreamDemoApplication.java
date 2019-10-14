6ad3982ef969c13f52ee0b8ff81dfd50376da651
Barry Tarlton <btarlton@gmail.com>
2019-10-14 15:36:31 -0400

    finally simple stream working

--- src/main/java/com/barry/demo/StreamDemoApplication.java
+++ src/main/java/com/barry/demo/StreamDemoApplication.java
@@ -1,25 +1,28 @@
 package com.barry.demo;
 
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.handler.annotation.SendTo;
 
 @SpringBootApplication
-@EnableBinding(Processor.class)
 public class StreamDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreamDemoApplication.class, args);
 	}
 
-	@StreamListener(Processor.INPUT)
-	@SendTo(Processor.OUTPUT)
-	public String upperCase(String value){
-		System.out.println(value);
-		return value.toUpperCase();
+	@Bean
+	public Function<String, String> upperCase(){
+		return value -> {
+			System.out.println(value);
+			return value.toUpperCase();
+		};
 	}
 
 	/* @Bean
